cc1c49a53e19ba20ebc35772aa4c330f45b25881
KiMinje <under201@gmail.com>
2025-09-22 16:06:48 +0900

    walk 크기 확대

--- animation_viewer.py
+++ animation_viewer.py
@@ -7,7 +7,7 @@ walk = load_image('SamuraiGirl_Walk.png')
 frame = 0
 for x in range(0, 800, 10):
     clear_canvas()
-    walk.clip_draw(frame * 165, 0, 165, 130, x + 165 // 2, 65)
+    walk.clip_draw(frame * 165, 0, 165, 130, x + 165 // 2, 65, 500, 500)
     update_canvas()
     frame = (frame + 1) % 8
     delay(0.05)
