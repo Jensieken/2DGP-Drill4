dfc444067d47f2c0c359b7fddba1f573edf5f6a6
KiMinje <under201@gmail.com>
2025-09-22 15:52:23 +0900

    walk 루프

--- animation_viewer.py
+++ animation_viewer.py
@@ -4,4 +4,12 @@ open_canvas()
 
 walk = load_image('SamuraiGirl_Walk.png')
 
+frame = 0
+for x in range(0, 800, 10):
+    clear_canvas()
+    walk.clip_draw(frame * 100, 0, 100, 100, x + 50, 90)
+    update_canvas()
+    frame = (frame + 1) % 8
+    delay(0.05)
+
 close_canvas()
